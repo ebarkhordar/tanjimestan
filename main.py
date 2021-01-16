803ec87183916d62dda63630e93f286faed2cca9
Ehsan Barkhordar <barkhordar.me@gmail.com>
2021-01-16 14:58:50 +0330

    remove print in main func

--- main.py
+++ main.py
@@ -64,7 +64,6 @@ def astropy_daily_report(context: telegram.ext.CallbackContext):
 
 def main():
     """Start the bot."""
-    print(datetime.datetime.now())
     # Create the Updater and pass it your bot's token.
     # Make sure to set use_context=True to use the new context based callbacks
     # Post version 12 this will no longer be necessary
