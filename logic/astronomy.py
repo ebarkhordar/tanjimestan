1f53c02f40fc7100892d69b42e0aeb00206782ee
Ehsan Barkhordar <barkhordar.me@gmail.com>
2021-03-23 23:59:38 +0430

    fix planets

--- logic/astronomy.py
+++ logic/astronomy.py
@@ -11,6 +11,9 @@ def get_planet_position(body='moon', specific_datetime=None):
         # today_date = datetime.now().strftime("2021-01-9 %H:%M")
     t = Time(specific_datetime)
     with solar_system_ephemeris.set('builtin'):
+        # 'earth', 'sun', 'moon', 'mercury', 'venus',
+        # 'earth-moon-barycenter', 'mars', 'jupiter',
+        # 'saturn', 'uranus', 'neptune'
         sky_coord = get_body(body, t)
         constellation = get_constellation(sky_coord)
         ra = sky_coord.ra
